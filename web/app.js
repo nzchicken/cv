fe0cbf96d55bd3f119a7757a32083772343e80b9
Ben Naylor <ben.naylor@beethree.nl>
2017-04-26 11:17:59 +0200

    add google analytics

--- web/app.js
+++ web/app.js
@@ -8,10 +8,16 @@ import './src/scss/styles.scss';
 const windowHandler = new WindowHandlers();
 const formHandler = new FormHandlers(data);
 
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,'script','https://www.google-analytics.com/analytics.js','ga'); 
+
 document.addEventListener('DOMContentLoaded', function() {
     var div = document.createElement('div');
     div.innerHTML = template(data);
     document.body.appendChild(div);
+
+    ga('create', data.analyticsTrackingCode, 'auto');
+    ga('send', 'pageview');
 
     windowHandler.registerHandlers();
     formHandler.registerHandlers();
