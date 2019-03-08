6653372c6a27f9221cea552bbd426fa9811470d5
Ben Naylor <nayben@gmail.com>
2019-03-08 12:13:36 +0100

    Updates

---
+++ pages-manifest-d0c6e62917d6926ce2bd.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{140:function(a){a.exports={pages:[{componentChunkName:"component---src-pages-index-js",jsonName:"index",path:"/"}],dataPaths:{"dev-404-page-5f9":"920/path---dev-404-page-5-f-9-fab-NZuapzHg3X9TaN1iIixfv1W23E",index:"234/path---index-6a9-Vctg0elmMNpCjTfBPjr41flap0"}}}}]);
+//# sourceMappingURL=pages-manifest-d0c6e62917d6926ce2bd.js.map
